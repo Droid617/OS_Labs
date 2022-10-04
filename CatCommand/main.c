f082a693e3a7854d08806daf0dd0d9010168788f
Droid617 <76783314+Droid617@users.noreply.github.com>
2022-10-04 13:55:30 +0300

    Update main.c

--- CatCommand/main.c
+++ CatCommand/main.c
@@ -79,7 +79,7 @@ int main(int argc, char** argv){
 	}
 		
 	else{
-		printf("cat: %s: No such file or directoryooo", fileName);
+		printf("cat: %s: No such file or directory", fileName);
 	}
 			
 	
